c78dc7ab6d20c43f840497d4e9e8415b7a2a4f54
jeus1998 <zeus20240228@gmail.com>
2024-09-23 17:36:46 +0900

    Spring Security 6-4 세션 관리: SessionManagementFilter / ConcurrentSessionFilter

--- src/main/java/spring/security/IndexController.java
+++ src/main/java/spring/security/IndexController.java
@@ -1,10 +1,17 @@
 package spring.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class IndexController {
+    private final SessionInfoService sessionInfoService;
+    @GetMapping("/sessionInfo")
+    public void sessionInfo(){
+        sessionInfoService.sessionInfo();
+    }
     @GetMapping("/")
     public String index(){
         return "index";
