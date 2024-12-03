ed5d8d02d769757c06d037d52f76867367116d3f
jeus1998 <zeus20240228@gmail.com>
2024-12-03 15:19:01 +0900

    [2024:12:03] 인증 아키텍처 - 인증 제공자 - AuthenticationProvider

--- src/main/java/spring/security/CustomAuthenticationProvider2.java
+++ src/main/java/spring/security/CustomAuthenticationProvider2.java
@@ -6,8 +6,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import java.util.List;
-
-public class CustomAuthenticationProvider implements AuthenticationProvider {
+public class CustomAuthenticationProvider2 implements AuthenticationProvider {
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         String loginId = authentication.getName();
--- src/main/java/spring/security/SecurityConfig.java
+++ src/main/java/spring/security/SecurityConfig.java
@@ -3,9 +3,11 @@ package spring.security;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -13,31 +15,38 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-// @EnableWebSecurity
-// @Configuration
+@EnableWebSecurity
+@Configuration
 @Slf4j
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
-        // build()는 최초 한번만 호출
-        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
-        // build()이후 getObject()로 참조해야 한다
-        AuthenticationManager authenticationManager2 = authenticationManagerBuilder.getObject();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationConfiguration configuration) throws Exception{
+
+        AuthenticationManagerBuilder managerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        managerBuilder.authenticationProvider(customAuthenticationProvider());
+
+        ProviderManager providerManager = (ProviderManager) configuration.getAuthenticationManager();
+        providerManager.getProviders().add(customAuthenticationProvider2());
+
         http
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/api/login").permitAll()
-                        .anyRequest().authenticated())
-                .authenticationManager(authenticationManager) // HttpSecurity 생성한 AuthenticationManager 저장
-                .addFilterBefore(customFilter(http, authenticationManager), UsernamePasswordAuthenticationFilter.class);
+            .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated());
+
+        //http.authenticationProvider(new CustomAuthenticationProvider2());
+
+        http
+            .formLogin(Customizer.withDefaults());
+
         return  http.build();
     }
-    public CustomAuthenticationFilter customFilter(HttpSecurity http, AuthenticationManager authenticationManager){
-        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(http);
-        customAuthenticationFilter.setAuthenticationManager(authenticationManager);
-        return customAuthenticationFilter;
+    @Bean
+    public CustomAuthenticationProvider customAuthenticationProvider(){
+        return new CustomAuthenticationProvider();
+    }
+    @Bean
+    public CustomAuthenticationProvider2 customAuthenticationProvider2(){
+        return new CustomAuthenticationProvider2();
     }
     @Bean
     public UserDetailsService userDetailsService(){
