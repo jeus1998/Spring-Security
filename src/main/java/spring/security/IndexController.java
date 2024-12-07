bd893ab24097ffba12b6820635cd789f4a335dc1
jeus1998 <zeus20240228@gmail.com>
2024-12-07 23:47:43 +0900

    [2024:12:07] 계층적 권한 - RoleHirerachy

---
+++ src/main/java/spring/security/IndexController.java
@@ -0,0 +1,24 @@
+package spring.security;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+    @GetMapping("/user")
+    public String user(){
+        return "user";
+    }
+    @GetMapping("/db")
+    public String db(){
+        return "db";
+    }
+    @GetMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
+}
