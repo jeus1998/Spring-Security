90b5a73d0f7747ea610cf1a5091fde30e050a3d5
jeus1998 <zeus20240228@gmail.com>
2024-12-07 13:48:19 +0900

    [2024:12:07] 요청기반권한부여 - authorizeHttpRequests

--- src/main/java/spring/security/IndexController.java
+++ src/main/java/spring/security/IndexController.java
@@ -1,16 +1,50 @@
 package spring.security;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequiredArgsConstructor
 public class IndexController {
-    @GetMapping("/")
-    public String index(){
-        return "index";
+    @GetMapping("/user")
+    public String user(){
+        return "user";
+    }
+
+    @GetMapping("/myPage/points")
+    public String myPage(){
+        return "myPage";
+    }
+
+    @GetMapping("/manager")
+    public String manager(){
+        return "manager";
+    }
+
+    @GetMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
+
+    @GetMapping("/admin/payment")
+    public String adminPayment(){
+        return "adminPayment";
+    }
+
+    @GetMapping("/resource/address_01")
+    public String address_01(){
+        return "address_01";
+    }
+
+    @GetMapping("/resource/address01")
+    public String address01(){
+        return "address01";
+    }
+
+    @PostMapping("/post")
+    public String post(){
+        return "post";
     }
 }
