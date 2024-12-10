0570dbaba2c3f4a671ae0114863dabb8742d32aa
jeus1998 <zeus20240228@gmail.com>
2024-12-10 23:12:12 +0900

    [2024:12:10] 포인트컷 메소드 보안 구현

--- src/main/java/spring/security/SecurityConfig.java
+++ src/main/java/spring/security/SecurityConfig.java
@@ -1,11 +1,15 @@
 package spring.security;
 
+import org.aopalliance.intercept.MethodInvocation;
 import org.springframework.aop.Advisor;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import org.springframework.aop.support.ComposablePointcut;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Role;
-import org.springframework.security.authorization.method.AuthorizationManagerAfterMethodInterceptor;
+import org.springframework.security.authorization.AuthorityAuthorizationManager;
 import org.springframework.security.authorization.method.AuthorizationManagerBeforeMethodInterceptor;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -17,7 +21,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = false)
@@ -25,16 +28,30 @@ import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 public class SecurityConfig {
     @Bean
     @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
-    public Advisor preAuthorize(){
-        return AuthorizationManagerBeforeMethodInterceptor.preAuthorize(new MyPreAuthorizationManager());
+    public Advisor pointCutAdvisor(){
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution(* spring.security.DataService.getUser(..))");
+        AuthorityAuthorizationManager<MethodInvocation> manager = AuthorityAuthorizationManager.hasRole("USER");
+        return new AuthorizationManagerBeforeMethodInterceptor(pointcut, manager);
     }
     @Bean
     @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
-    public Advisor postAuthorize(){
-        return AuthorizationManagerAfterMethodInterceptor.postAuthorize(new MyPostAuthorizationManager());
+    public Advisor pointCutAdvisor2(){
+        AspectJExpressionPointcut pointcut1 = new AspectJExpressionPointcut();
+        pointcut1.setExpression("execution(* spring.security.DataService.getUser(..))");
+
+        AspectJExpressionPointcut pointcut2 = new AspectJExpressionPointcut();
+        pointcut2.setExpression("execution(* spring.security.DataService.getOwner(..))");
+
+        ComposablePointcut composablePointcut = new ComposablePointcut((Pointcut) pointcut1);
+        composablePointcut.union((Pointcut) pointcut2);
+
+        AuthorityAuthorizationManager<MethodInvocation> manager = AuthorityAuthorizationManager.hasRole("USER");
+        return new AuthorizationManagerBeforeMethodInterceptor(composablePointcut, manager);
     }
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http
                 .authorizeHttpRequests(authorize -> authorize
                         .anyRequest().authenticated())
