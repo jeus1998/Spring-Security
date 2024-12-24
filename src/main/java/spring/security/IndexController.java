8e845bd91bd3347a185056ea5cabc2417197308e
jeus1998 <zeus20240228@gmail.com>
2024-12-24 18:06:56 +0900

    [2024:12:24] 스프링 MVC 비동기 통합

--- src/main/java/spring/security/IndexController.java
+++ src/main/java/spring/security/IndexController.java
@@ -1,45 +1,26 @@
 package spring.security;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.io.IOException;
-import java.util.Collections;
-import java.util.List;
-
+@Slf4j
 @RestController
+@RequiredArgsConstructor
 public class IndexController {
+    private final AsyncService asyncService;
     @GetMapping("/")
-    public String index(HttpServletRequest request){
+    public String index(){
+        Authentication authentication = SecurityContextHolder.getContextHolderStrategy().getContext().getAuthentication();
+        log.info("currentThread={}", Thread.currentThread().getName());
+        log.info("authentication={}", authentication);
+        asyncService.async();
         return "index";
     }
     @GetMapping("/user")
     public String user(){
         return "user";
-    }
-    @GetMapping("/db")
-    public String db(){
-        return "db";
-    }
-    @GetMapping("/admin")
-    public String admin(){
-        return "admin";
-    }
-
-    @GetMapping("/login")
-    public String login(HttpServletRequest request, MemberDto memberDto) throws ServletException, IOException {
-        request.login(memberDto.getUsername(), memberDto.getPassword());
-        System.out.println("login is successful");
-        return "login";
-    }
-    @GetMapping("/users")
-    public List<MemberDto> users(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        boolean authenticate = request.authenticate(response);
-        if (authenticate) {
-            return List.of(new MemberDto("user","1111"));
-        }
-        return Collections.emptyList();
     }
 }
--- src/main/java/spring/security/SecurityConfig.java
+++ src/main/java/spring/security/SecurityConfig.java
@@ -3,10 +3,12 @@ package spring.security;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -28,8 +30,10 @@ public class SecurityConfig {
                         .requestMatchers("/db").hasAuthority("ROLE_DB")
                         .requestMatchers("/admin").hasAuthority("ROLE_ADMIN")
                         .anyRequest().permitAll())
-                //.formLogin(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable);
+
+        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
 
         return http.build();
     }
